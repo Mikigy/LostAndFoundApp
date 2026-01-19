1503d50eb359ee88262213b91aa608f5339ac6ed
Mikiale Goitom <73868256+Mikigy@users.noreply.github.com>
2026-01-19 08:52:23 -0800

    Add function declarations in lostfound.h
    Added function declarations for managing lost items.

--- include/lostfound.h
+++ include/lostfound.h
@@ -1,1 +1,19 @@
+#ifndef LOSTFOUND_H
+#define LOSTFOUND_H
+
+const int MAX = 100;
+
+// Function declarations
+void addItem(int*, char[][20], char[][20], char[][20], char[][15],
+             char[][20], char[][30], char[][30],
+             char[][15], char[][15], int*);
+
+void viewLostItems(int*, char[][20], char[][20], char[][20], char[][15], int*);
+void searchItem(char[][20], char[][20], char[][20], char[][15], int*);
+void claimItem(int*, char[][20], char[][30], char[][15], int*);
+void viewHistory(int*, char[][20], char[][20], char[][20], char[][15],
+                 char[][30], char[][30], char[][15], char[][15], int*);
+void toLower(char*);
+
+#endif
 

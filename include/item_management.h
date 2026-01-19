8638bdfefbc1f7bd6a3d300dac10bfffc88bcec8
Mikiale Goitom <73868256+Mikigy@users.noreply.github.com>
2026-01-19 08:50:52 -0800

    Add item management header file

---
+++ include/item_management.h
@@ -0,0 +1,10 @@
+#ifndef ITEM_MANAGEMENT_H
+#define ITEM_MANAGEMENT_H
+
+void addItem(int*, char[][20], char[][20], char[][20], char[][15],
+             char[][20], char[][30], char[][30],
+             char[][15], char[][15], int*);
+
+void viewLostItems(int*, char[][20], char[][20], char[][20], char[][15], int*);
+
+#endif
